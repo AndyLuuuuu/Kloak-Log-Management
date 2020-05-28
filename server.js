bf1e4c4e2e9cb0fef254e1ef447f28d2ae49e675
Andy Lu <yilunglu.andy@gmail.com>
2020-05-28 16:12:08 -0700

    Changed app.use for views

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/views/index.html'));
+    res.sendFile('index.html');
 });
 app.listen(PORT, function () {
     console.log("Server is up on PORT " + PORT);
--- server.ts
+++ server.ts
@@ -4,9 +4,10 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'views')))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/index.html'))
+  res.sendFile('index.html')
 })
 
 app.listen(PORT, () => {
